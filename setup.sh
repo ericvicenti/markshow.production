8375a8fb106a1542a5149d27c41e0f53f827bf27
Eric Vicenti <ericvicenti@gmail.com>
2012-10-28 15:06:19 -0700

    Add chowns

--- setup.sh
+++ setup.sh
@@ -23,10 +23,18 @@ setup_path=`pwd`
 # # Install configs
 sudo service monit stop
 sudo service markshow stop
+
 sudo cp -f $setup_path/upstart.conf /etc/init/markshow.conf
+sudo chown root:root /etc/init/markshow.conf
+
 sudo cp -f $setup_path/monitrc /etc/monit/monitrc
+sudo chown root:root /etc/monit/monitrc
+
 sudo cp -f $setup_path/nginx/vhost.conf /etc/nginx/sites-available/markshow.org.conf
+sudo chown root:root /etc/nginx/sites-available/markshow.org.conf
+
 sudo ln -s /etc/nginx/sites-available/markshow.org.conf /etc/nginx/sites-enabled/markshow.org.conf
+
 sudo service ngnix restart
 sudo service monit start
 sudo service markshow start
